715cd89671d30268b975ae688dc7b6df0e67cec6
Sourav Sharma <67095793+srv9211@users.noreply.github.com>
2022-10-05 07:14:15 +0530

    Add files via upload

---
+++ Project/Models/MyComponentInterface.java
@@ -0,0 +1,10 @@
+// for testing purpose
+
+package com.akqa.aem.training.aem201.core.models;
+
+public interface MyComponentInterface {
+    String getFirstName();
+    String getLastName();
+    String getAddress();
+    String getPageTitle();
+}
